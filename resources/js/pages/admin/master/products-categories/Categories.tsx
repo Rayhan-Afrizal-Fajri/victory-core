70a4bbec29629e4024820a02bfe165883c27b1ee
R230905 <afrizalfajri23@gmail.com>
2026-07-27 21:26:47 +0700

    fix: filtering material in detail product

--- resources/js/pages/admin/master/products-categories/Categories.tsx
+++ resources/js/pages/admin/master/products-categories/Categories.tsx
@@ -64,8 +64,6 @@ export default function Categories({ categories, bahanMaterials, aksesorisMateri
     manufacturing_work_ids: [] as number[],
   });
 
-  console.log(aksesorisMaterials);
-
   const openCreateDialog = () => {
     reset();
     setEditingId(null);
@@ -284,7 +282,7 @@ export default function Categories({ categories, bahanMaterials, aksesorisMateri
                         }
                       />
                       <Label htmlFor={`bahan-${bahan.id}`} className="font-normal cursor-pointer">
-                        {bahan.name} - {bahan.default_vendor.nama_perusahaan || bahan.default_vendor.nama || '-' }
+                        {bahan.name} - {bahan.default_vendor?.nama_perusahaan || bahan.default_vendor?.nama || '-' }
                       </Label>
                     </div>
                   )
@@ -307,7 +305,7 @@ export default function Categories({ categories, bahanMaterials, aksesorisMateri
                         }
                       />
                       <Label htmlFor={`aks-${aksesoris.id}`} className="font-normal cursor-pointer">
-                        {aksesoris.name} - {aksesoris.default_vendor.nama_perusahaan || aksesoris.default_vendor.nama || '-' }
+                        {aksesoris.name} {(aksesoris.default_vendor?.nama_perusahaan || aksesoris.default_vendor?.nama) && ('-')} {aksesoris.default_vendor?.nama_perusahaan || aksesoris.default_vendor?.nama || '-' }
                       </Label>
                     </div>
                   ))}
